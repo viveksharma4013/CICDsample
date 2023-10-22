84b69727ccc24264939e52b9dbb34076a2d794d8
viveksharma4013 <56532554+viveksharma4013@users.noreply.github.com>
2023-10-22 20:55:17 +0530

    first commit

---
+++ src/test/java/com/CICDSample/cicdsample/CicdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICDSample.cicdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
